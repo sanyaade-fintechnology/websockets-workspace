d6c63dd384a7ef20d8765c9a9999c1248a431743
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-04-08 18:53:13 +0200

    add open/close/error event method handlers

--- EchoServer/src/main/java/com/websockets/EchoServer.java
+++ EchoServer/src/main/java/com/websockets/EchoServer.java
@@ -1,16 +1,36 @@
 package com.websockets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.websocket.OnMessage;
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 
 @Service
 @ServerEndpoint("/echo")
 public class EchoServer {
 
+    private static Logger LOG = LoggerFactory.getLogger(EchoServer.class);
+
     @OnMessage
     public String echo(String incomingMessage) {
+        LOG.info("Received message: " + incomingMessage);
         return "I got this (" + incomingMessage + ") so I'm sending it back!";
+    }
+
+    @OnOpen
+    public void init(Session session) {
+        LOG.info("Opening connection, session id: {}", session.getId());
+    }
+
+    @OnClose
+    public void onClose(CloseReason closeReason) {
+        LOG.info("Closing connection, reason: {}", closeReason.getCloseCode());
+    }
+
+    @OnError
+    public void errorHandler(Throwable err) {
+        LOG.error("WebSocket error!", err);
     }
 }
